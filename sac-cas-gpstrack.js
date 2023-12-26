2f735531baf46d2d8c1a63e9db9d43859f9aa22b
Oleg Demakov <odemakov@gmail.com>
2023-12-26 21:11:22 +0100

    create all the tracks in the result gpx file, even though Strava/Garmon will use only first one

--- sac-cas-gpstrack.js
+++ sac-cas-gpstrack.js
@@ -1,56 +1,16 @@
 
-document.body.style.border = "5px solid green";
-
 window.addEventListener('load',function() {
-    console.log("removeButton")
-    removeButton();
-    console.log("addButton");
+    clean();
     addButton();
 });
 
-
-
-function download() {
-    console.log("download");
-    fetch('https://www.sac-cas.ch/en/?type=1567765346410&tx_usersaccas2020_sac2020[routeId]=4205')
-        .then(response => response.json()) // Parse the JSON in the response
-        .then(data => {
-			const route_type = data.segments[0].route_type;
-			let points = data.segments[0].geom.coordinates.map(
-				(c) => Swisstopo.toTrkpt(c[1], c[0])
-			);
-			console.log(data.segments[0]);
-            const gpx = createXmlString(points);
-            const url = 'data:text/json;charset=utf-8,' + gpx;
-            const link = document.createElement('a');
-            link.download = `SAC-CAS-${route_type}-export.gpx`;
-            link.href = url;
-            document.body.appendChild(link);
-            link.click();
-            // You can now interact with 'data' in the console or perform further operations
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-function createXmlString(points) {
-    let result = '<?xml version="1.0" encoding="UTF-8"?>'
-    result += '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="runtracker">'
-    result += '<trk>';
-    result += '<name>Morning Trail Run</name>';
-    result += '<type>running</type>';
-    result += '<trkseg>';
-    points.forEach((point) => {
-        result += `<trkpt lat="${point[1]}" lon="${point[0]}"></trkpt>`;
-    });
-    result += '</trkseg>'
-    result += '</trk>';
-    result += '</gpx>';
-    return result;
-}
-
-function removeButton() {
-    const elements = document.getElementsByClassName("sac-cas-gpstrack");
-    while (elements.length > 0) elements[0].remove();
+function clean() {
+    // clean button
+    const buttons = document.getElementsByClassName("sac-cas-gpstrack-button");
+    while (buttons.length > 0) buttons[0].remove();
+    // clean link
+    const links = document.getElementsByClassName("sac-cas-gpstrack-link");
+    while (links.length > 0) links[0].remove();
 }
 
 function addButton() {
@@ -60,35 +20,92 @@ function addButton() {
     svg.setAttribute("aria-hidden", true);
     svg.className = "m-map__control-icon svg";
 
+    const path = window.location.pathname.split("/");
     const button = document.createElement("button");
-    button.className = "m-map__control sac-cas-gpstrack";
+    button.className = "m-map__control sac-cas-gpstrack-button";
     button.setAttribute('type', 'button');
     button.textContent = 'GPS';
     button.addEventListener("click", download);
-	button.setAttribute("style", "background-color: blanchedalmond;")
+    button.setAttribute("style", "background-color: blanchedalmond;")
+    button.setAttribute("data-route-id", path[path.length-1]);
 
     el.appendChild(button);
 }
 
-var Swisstopo = {
-	toTrkpt(east, north) {
-    	if (north >= 1000000 && east >= 1000000) {
-        	if (north >= 2000000) {
-            	const big = north;
-            	north = east - 1000000;
-	            east = big - 2000000;
-    	    } else if (east >= 2000000) {
-        	    north -= 1000000;
-            	east -= 2000000;
-	        } else {
-    	        throw "Number format exception";
-        	}
-    	}
-    	if (north < 0 || north > 1000000 || east < 0 || east > 1000000) {
-        	throw "Number format exception";
-    	}
-    	return Swisstopo.CHtoWGS(east, north);
-	},
+function download(event) {
+    const routeId = event.target.dataset.routeId;
+    const url = `https://www.sac-cas.ch/en/?type=1567765346410&tx_usersaccas2020_sac2020[routeId]=${routeId}`
+    fetch(url)
+        .then(response => response.json()) // Parse the JSON in the response
+        .then(data => {
+            let gpx = new GPXString(data.title, data.type, document.location);
+            data.segments.map((segment) => {
+                gpx.addSegment(segment.geom.coordinates.map(
+                    (c) => Swisstopo.toTrkpt(c[1], c[0])
+                ));
+            });
+            const link = document.createElement('a');
+            link.download = `SAC-CAS-${routeId}-${slugify(data.title)}.gpx`;
+            link.href = "data:text/json;charset=utf-8," + gpx;
+            link.className = "sac-cas-gpstrack-link";
+            link.setAttribute("style", "display:none;visibility:hidden");
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+function slugify(str) {
+    return str.toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, '')
+        .replace(/[\s_-]+/g, '-')
+        .replace(/^-+|-+$/g, '');
+}
+
+const GPXString = class {
+    constructor(name, type, src) {
+        this.gpx = '<?xml version="1.0" encoding="UTF-8"?>'
+        this.gpx += '<gpx xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd" version="1.1" creator="SAC-CAS-GPSTrack">'
+        this.gpx += '<trk>';
+        this.gpx += `<name>${name}</name>`;
+        this.gpx += `<type>${type}</type>`;
+        this.gpx += `<src>${type}</src>`;
+    }
+    addSegment(points) {
+        this.gpx += '<trkseg>';
+        points.forEach((point) => {
+            this.gpx += `<trkpt lat="${point[1]}" lon="${point[0]}"></trkpt>`;
+        });
+        this.gpx += '</trkseg>';
+    }
+    toString() {
+        this.gpx += '</trk>';
+        this.gpx += '</gpx>';
+        return this.gpx;
+    }
+}
+
+const Swisstopo = {
+    toTrkpt: function(east, north) {
+        if (north >= 1000000 && east >= 1000000) {
+            if (north >= 2000000) {
+                const big = north;
+                north = east - 1000000;
+                east = big - 2000000;
+            } else if (east >= 2000000) {
+                north -= 1000000;
+                east -= 2000000;
+            } else {
+                throw "Number format exception";
+            }
+        }
+        if (north < 0 || north > 1000000 || east < 0 || east > 1000000) {
+            throw "Number format exception";
+        }
+        return Swisstopo.CHtoWGS(east, north);
+    },
 
     WGStoCH: function (lat, lng) {
         return [
@@ -150,11 +167,11 @@ var Swisstopo = {
 
         // Process lat
         const lat = 16.9023892 +
-            3.238272 * x_aux -
-            0.270978 * Math.pow(y_aux, 2) -
-            0.002528 * Math.pow(x_aux, 2) -
-            0.0447       * Math.pow(y_aux, 2) * x_aux -
-            0.0140       * Math.pow(x_aux, 3);
+              3.238272 * x_aux -
+              0.270978 * Math.pow(y_aux, 2) -
+              0.002528 * Math.pow(x_aux, 2) -
+              0.0447       * Math.pow(y_aux, 2) * x_aux -
+              0.0140       * Math.pow(x_aux, 3);
 
         // Unit 10000" to 1 " and converts seconds to degrees (dec)
         return lat * 100 / 36;
@@ -169,10 +186,10 @@ var Swisstopo = {
 
         // Process lng
         const lng = 2.6779094 +
-            4.728982 * y_aux +
-            0.791484 * y_aux * x_aux +
-            0.1306       * y_aux * Math.pow(x_aux, 2) -
-            0.0436       * Math.pow(y_aux, 3);
+              4.728982 * y_aux +
+              0.791484 * y_aux * x_aux +
+              0.1306       * y_aux * Math.pow(x_aux, 2) -
+              0.0436       * Math.pow(y_aux, 3);
 
         // Unit 10000" to 1 " and converts seconds to degrees (dec)
         return lng * 100 / 36;
